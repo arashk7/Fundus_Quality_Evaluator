dfa06ac59b18061d5437a811573eb501d7879f92
Ahmad Karambakhsh <ahmad.karambakhsh@gmail.com>
2021-02-05 16:18:14 +0800

    Update cla_isbi_streamlit.py

--- clarity/cla_isbi_streamlit.py
+++ clarity/cla_isbi_streamlit.py
@@ -14,7 +14,8 @@ ISBI_TEST_PATH = 'E:\Dataset\DR\DeepDr\Onsite-Challenge1-2-Evaluation'
 
 
 '''Sample image path'''
-image_path = "E:\Dataset\DR\DeepDr\Onsite-Challenge1-2-Evaluation/13/13_l1.jpg"
+image_path1 = "E:\Dataset\DR\DeepDr\Onsite-Challenge1-2-Evaluation/39/39_r2.jpg" # 41_l1
+image_path0 = "E:\Dataset\DR\DeepDr\Onsite-Challenge1-2-Evaluation/41/41_l1.jpg"
 
 ''' Instanciate an object from FQEModel'''
 model = CLAModel()
@@ -25,8 +26,8 @@ model = CLAModel()
 # model.eff_model = torch.load('checkpoints_isbi/_ckpt_epoch_3.ckpt')
 # model.load_state_dict(torch.load('checkpoints_isbi/_ckpt_epoch_3.ckpt'))
 # m = torch.load('checkpoints_isbi/_ckpt_epoch_3.ckpt')
-# model.load_state_dict("checkpoints_isbi/fqe_isbi_acc_1.0_fold_3.pt")
-model.load_from_checkpoint("checkpoints_isbi/clarity_isbi_acc_0.0_fold_1.pt")#'checkpoints_isbi/fqe_isbi_acc_1.0_fold_2.pt')
+model.load_state_dict(torch.load("checkpoints_isbi/SAN-340_loss_0.062_fold_2.pt"))
+# model.load_from_checkpoint("checkpoints_isbi/SAN-340_loss_0.062_fold_2.pt")#'checkpoints_isbi/fqe_isbi_acc_1.0_fold_2.pt')
 # model.freeze()
 print(model.summarize())
 
@@ -34,9 +35,9 @@ def test_isbi(trainer, model, dataset_test):
     test_loader = torch.utils.data.DataLoader(dataset_test,  # num_workers= 16,
                                               batch_size=batch_size)
     result = trainer.test(model, test_loader)
-    acc = result[0]['test_epoch_acc']
+    acc = result[0]['test_loss']
     acc = round(acc, 3)
-    print('>>>>>>>>>>>>>>>>Test acc: ' + str(acc))
+    print('>>>>>>>>>>>>>>>>Test loss: ' + str(acc))
 
 
 '''Transform the image'''
@@ -55,28 +56,34 @@ trainer = pl.Trainer(gpus=1)
 
 
 '''Load a test image'''
-img = Image.open(image_path)
+img0 = Image.open(image_path0)
+img1 = Image.open(image_path1)
 
 
 
-img = transform_test(img)
+img0 = transform_test(img0)
+img1 = transform_test(img1)
 # img2 = transform_test(img)
 # img3 = transform_test(img)
 # img4 = transform_test(img)
 
 
 ''' Unsqueeze batch dimension, in case you are dealing with a single image'''
-img = img.unsqueeze(0)
+img0 = img0.unsqueeze(0)
+img1 = img1.unsqueeze(0)
 # batch = torch.Tensor([img,img,img,img])
 # batch = next(iter([img,img,img,img]))
-c = torch.cat((img, img,img,img), 0)
-
-
+# c = torch.cat((img, img,img,img), 0)
+#
+model.freeze()
 ''' Predict the quality of Fundus image'''
 
-out = model.predict(img)
+out = model.predict(img0)
+print(out.item())
+out = model.predict(img1)
+print(out.item())
 # pred = torch.reshape(out.argmax(dim=1, keepdim=True), ( 4,1))
 
-print(out)
+
 
 
